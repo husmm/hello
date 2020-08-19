6885fbe9ceeeb7ecfea3b2a0cf91b62ccb6a27de
husmm <yimeng@huskers.unl.edu>
2020-08-19 16:57:36 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Yimeng Zhao  
+ *         yimeng@huskers.unl.edu
+ * Date: 2020/8/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Yimeng Zhao\n");
+
+  return 0;
+}
